6434579530e93db893d6e53800d2feb811d26fbf
Federica Di Lauro <federicadilauro1998@gmail.com>
2022-07-21 10:40:50 +0200

    add option to dump correlation to csv files

--- phaser_core/src/backend/uncertainty/phase-correlation-eval.cc
+++ phaser_core/src/backend/uncertainty/phase-correlation-eval.cc
@@ -1,6 +1,12 @@
 #include "phaser/backend/uncertainty/phase-correlation-eval.h"
 
+#include <fstream>
 #include <glog/logging.h>
+#include <iterator>
+
+DEFINE_bool(
+    dump_correlation_to_file, false,
+    "Decide to dump the correlation to csv or not");
 
 namespace phaser_core {
 
@@ -13,6 +19,16 @@ common::BaseDistributionPtr PhaseCorrelationEval::calcRotationUncertainty(
     const SphericalCorrelation& sph_corr) {
   const uint32_t bw = sph_corr.getBandwidth();
   const std::vector<double> corr = sph_corr.getCorrelation();
+
+  if (FLAGS_dump_correlation_to_file) {
+    std::ofstream file;
+    file.open("rotation_correlation.csv");
+    std::copy(
+        corr.begin(), corr.end(), std::ostream_iterator<double>(file, "\n"));
+    file.flush();
+    file.close();
+    LOG(INFO) << "Dumped translation correlation to file";
+  }
   return rotation_eval_->evaluateCorrelationFromRotation(bw, corr);
 }
 
@@ -22,6 +38,15 @@ common::BaseDistributionPtr PhaseCorrelationEval::calcTranslationUncertainty(
   const uint32_t n_voxels = aligner.getNumberOfVoxels();
   const uint32_t lower_bound = aligner.getLowerBound();
   const uint32_t upper_bound = aligner.getUpperBound();
+  if (FLAGS_dump_correlation_to_file) {
+    std::ofstream file;
+    file.open("translation_correlation.csv");
+    std::copy(
+        corr.begin(), corr.end(), std::ostream_iterator<double>(file, "\n"));
+    file.flush();
+    file.close();
+    LOG(INFO) << "Dumped translation correlation to file";
+  }
   VLOG(1) << "----------- Computing translation with " << n_voxels << " voxels";
   return positional_eval_->evaluateCorrelationFromTranslation(
       n_voxels, lower_bound, upper_bound, corr);
